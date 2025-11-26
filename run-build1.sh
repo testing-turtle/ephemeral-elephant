80891a922f8692868637b664f644a585bf0833db
Stuart Leeks <stuartle@microsoft.com>
2025-11-26 09:12:12 +0000

    Add workflow run-name and batch id

--- run-build1.sh
+++ run-build1.sh
@@ -6,9 +6,11 @@ num_workflows=20
 sleep_duration=300 # 5 minutes
 workflow_name="build1.yml"
 
+batch_id=$(date +%s)
+
 for i in $(seq 1 $num_workflows); do
   echo "Starting workflow build$i"
-  gh workflow run "$workflow_name" --ref main -f workflow_identifier=build$i -f sleep_duration=$sleep_duration
+  gh workflow run "$workflow_name" --ref main -f batch_identifier=$batch_id -f workflow_identifier=build$i -f sleep_duration=$sleep_duration
 done
 
 sleep 3
--- run-build2.sh
+++ run-build2.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -e
 
-num_workflows=20
+num_workflows=10
 # sleep_duration=120 # 2 minutes
 sleep_duration=300 # 5 minutes
 workflow_name="build2.yml"
 
+batch_id=$(date +%s)
+
 for i in $(seq 1 $num_workflows); do
   echo "Starting workflow build$i"
-  gh workflow run "$workflow_name" --ref main -f workflow_identifier=build$i -f sleep_duration=$sleep_duration
+  gh workflow run "$workflow_name" --ref main -f batch_identifier=$batch_id -f workflow_identifier=build$i -f sleep_duration=$sleep_duration
 done
 
 sleep 3
 
 gh run list --workflow "$workflow_name" --limit $num_workflows
-
+# gh run list --workflow "$workflow_name" --limit $num_workflows --json databaseId,headBranch,headSha,runNumber,status,conclusion,createdAt,updatedAt,event,workflowName --jq '.[] | {runNumber, status, conclusion, createdAt, updatedAt, event, workflowName}' | tee runs.json
